37791543313e8e18f955859d78fc1ae9b103cbba
Jacob Brady <jacobbrady@Jacob-Bradys-MacBook-Pro.local>
2014-12-26 16:06:07 +0000

    changed packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.4
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Jacob Brady',
+      author_email='jacob.brady0449@gmail.com',
+      url='https://github.com/j-brady/BioTools',
+      packages=['BioTools'],
+)
